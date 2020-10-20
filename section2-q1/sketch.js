aa649deaaa5222ea42e5c0aeb0787b0f19c01168
KokoroMatsushita <72027031+KokoroMatsushita@users.noreply.github.com>
2020-10-20 15:41:37 +0900

    section2-q1

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,14 @@
 // 小手調べ
 function setup(){
-  for(let i = 0; i < 10; i++){
-    // BLANK[1]
+  createCanvas(110,110);
+  for(let i = 10; i < 60; i=i+10){
+stroke(255,0,0);
+noFill();
+  ellipse(50,50,i)  // BLANK[1]
+  }
+  for(let j=60; j<110; j=j+10){
+    stroke(0,0,255);
+    noFill();
+    ellipse(50,50,j)
   }
 }
