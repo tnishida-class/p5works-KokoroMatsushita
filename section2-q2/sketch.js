b28884463e973a8beb9008a43bb25831888cc791
KokoroMatsushita <72027031+KokoroMatsushita@users.noreply.github.com>
2020-10-24 16:41:22 +0900

    2-q

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -2,9 +2,16 @@
 function setup() {
   createCanvas(200, 200);
   let size = width / 8;
-  noStroke();
-  for(let i = 0; i < 8; i++){
-    for(let j = 0; j < 8; j++){
+  noStroke()
+
+  for(let i = 1; i < 8; i++){
+    for(let j = 1; j < 8; j++){
+      console.log(i,j);
+      rect(size*i,size*j,size,size);
+      noFill();
+      if((i+j)%2==0){
+        fill(196);
+      }
       // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
     }
   }
