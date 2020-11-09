b2e5c8877b06fa584b50a81d3a9146b51a9242be
KokoroMatsushita <72027031+KokoroMatsushita@users.noreply.github.com>
2020-11-10 03:18:33 +0900

    section3

--- section3-1/sketch.js
+++ section3-1/sketch.js
@@ -1,12 +1,25 @@
 // テキスト「関数を使う」
 // 練習問題：このプログラムを改造してEUの旗を描いてみよう
 function setup(){
-  createCanvas(200, 200);
-  background(192);
+  createCanvas(300, 200);
+  background(51,51,255);
+fill(255,204,0)
+  noStroke();
   for(let i = 0; i < 12; i++){
     let theta = TWO_PI * i / 12;
-    let x = 100 + cos(theta) * 50;
+    let x = 150 + cos(theta) * 50;
     let y = 100 + sin(theta) * 50;
-    ellipse(x, y, 10);
+    star(x,y,10);
   }
 }
+
+  function star(cx,cy,r){
+    beginShape();
+    for(var i = 0; i < 5; i++){
+      let theta = TWO_PI * i * 2 / 5 - HALF_PI;
+      let x = cx + cos(theta) * r;
+      let y = cy + sin(theta) * r;
+      vertex(x,y);
+    }
+    endShape(CLOSE);
+  }
--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -4,15 +4,15 @@
 function setup(){
   createCanvas(400, 400);
   background(255);
-  balloon("I love keyakizaka46");
+  balloon("I love keyakizaka46",20,30,0,300);
 }
 
-function balloon(t){
+function balloon(t,x,y,bc,tc){
   let w = textWidth(t);
-  let h = textAscent() + textDescent();
+  let h = textAscent() + textDescent(); //文字列の高さを表している（下に突き出すか上に突き出すか）
   let p = 2;
-  fill(0);
+  fill(bc);
   rect(0, 0, w + p * 2, h + p * 2);
-  fill(255);
+  fill(tc);
   text(t, p, h + p);
 }
