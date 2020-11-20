6d102a0448a50d79f3cd9a9134be3107331ce15c
KokoroMatsushita <72027031+KokoroMatsushita@users.noreply.github.com>
2020-11-20 23:25:32 +0900

    finalwork

--- finalwork/sketch.js
+++ finalwork/sketch.js
@@ -1,36 +1,37 @@
 // 最終課題を制作しよう
-let x , y ;
+let x , y,vx, vy;
+const g = 0; // 重力加速度
+const vyMax = 30; ;
+
 function setup(){
   createCanvas(windowWidth, windowHeight);
-  x = windowWidth-60;
-  y = windowHeight-95
-}
-
-function draw(){
-  background(102, 204, 255);
-  fill(0)
-  strokeWeight(3)
-  line(0,windowHeight-50,windowWidth,windowHeight-50)
-  fill(255);
-  textSize(100);
-  text("よけろ！",windowWidth /2　,windowHeight/2)
-  image(img2, 0, 0)
-  image(img, x , y,60,50);
-  if(keyIsDown(LEFT_ARROW)){ x -= 5; }
-  if(keyIsDown(RIGHT_ARROW)){ x += 5; }
-  if(keyIsDown(UP_ARROW)){ y -= 5 ; }
-  if(keyIsDown(DOWN_ARROW)){ y += 5 ; }
+  x = windowWidth - 60;
+  y = windowHeight - 95;
+  vx = 2;
+  vy = 1;
 }
 function windowResized(){
   resizeCanvas(windowWidth, windowHeight);
 }
+function draw(){
+  background(102, 204, 255);
+  let c = color(230, 172, 0);
+  fill(c);
+  noStroke();
+  rect(0, windowHeight-50, windowWidth, 50);
+  image(img, x , y,60,50);
+  y += vy;
+  vy += g;
+  vy = constrain(vy, -vyMax, vyMax);
+  if(y < 0 || y > windowHeight-100){ vy = 1 * vy; }//どうやったら地面で跳ね返る？
+  y = constrain(y, 0, height);
+  if(keyIsDown(LEFT_ARROW)){ x -= 5; }
+  if(keyIsDown(RIGHT_ARROW)){ x += 5; }
+  if(keyIsDown(SHIFT)){y -= 5 }
+}
+
 
 let img;
 function preload() {
   img = loadImage('eto_remake_inoshishi.png');
-}
-
-let img2;
-function preload() {
-  img = loadImage('kuri.png');
 }
