0287fd36d33f90ac62570c935ea8ad5c78f05d1c
KokoroMatsushita <72027031+KokoroMatsushita@users.noreply.github.com>
2020-11-21 17:30:21 +0900

    finalwork

--- finalwork/sketch.js
+++ finalwork/sketch.js
@@ -1,33 +1,55 @@
 // 最終課題を制作しよう
-let x , y,vx, vy;
-const g = 0; // 重力加速度
-const vyMax = 30; ;
+let x,y,vx,vy,a,b;
+let animate
+const g = 2; // 重力加速度
+const vyMax = 200;
 
 function setup(){
   createCanvas(windowWidth, windowHeight);
   x = windowWidth - 60;
-  y = windowHeight - 95;
+  y = windowHeight/2;
   vx = 2;
   vy = 1;
+  a = 0
+  b = windowHeight/2;
+  animete = false;
 }
 function windowResized(){
   resizeCanvas(windowWidth, windowHeight);
 }
+
+//let h =[];
+//for(let i = 0; i<10, i++1){
+ //h[i] = random(0,windowHeight-110);
+//}
+//for(let i = 0; i< h.length; i++){
+ //const dx
+ //rect(a,r,80,60)
+ //if(animate) a +=5
+
+
 function draw(){
   background(102, 204, 255);
   let c = color(230, 172, 0);
   fill(c);
   noStroke();
   rect(0, windowHeight-50, windowWidth, 50);
-  image(img, x , y,60,50);
-  y += vy;
-  vy += g;
+  fill(255)
+  //rect(a,b,80,60)
+  //if(animate) a +=5
+  image(img, x , y,120,100);
+  if(animate)y += 2
+  if(animate) vy += g;
   vy = constrain(vy, -vyMax, vyMax);
-  if(y < 0 || y > windowHeight-100){ vy = 1 * vy; }//どうやったら地面で跳ね返る？
-  y = constrain(y, 0, height);
-  if(keyIsDown(LEFT_ARROW)){ x -= 5; }
-  if(keyIsDown(RIGHT_ARROW)){ x += 5; }
-  if(keyIsDown(SHIFT)){y -= 5 }
+  if(y < 0 || y > windowHeight-200){ vy = 1 * vy; }
+  y = constrain(y, 0, height-130);
+  if(animate) x -=2
+  x = constrain(x,0,width);
+}
+
+function keyPressed(){
+  if(keyCode == SHIFT){y -= 50}
+  if(keyCode == ENTER){animate = !animate;}
 }
 
 
